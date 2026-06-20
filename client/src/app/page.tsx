1b803577391b578cb3e5a6c1f54a5767b3bfae70
Pedro Casseb <casseb.phcc@gmail.com>
2026-06-20 16:25:29 -0300

    feat(client): implement cart state, product selection, and checkout forms

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,11 +1,18 @@
+import ProductList from "@/components/ProductList";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Homepage({
+  searchParams,
+}: {
+  searchParams: Promise<{ category: string }>;
+}) {
+  const category = (await searchParams).category;
   return (
     <div className="">
       <div className="relative aspect-3/1 mb-12">
-        <Image src="/featured.png" alt="Feartured Product" fill />
+        <Image src="/featured.png" alt="Featured Product" fill />
       </div>
+      <ProductList category={category} params="homepage" />
     </div>
   );
 }
