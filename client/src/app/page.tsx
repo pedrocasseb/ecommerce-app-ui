f40324dc4e1f6439a2c4872d3d582abb5250372e
Pedro Casseb <casseb.phcc@gmail.com>
2026-06-20 15:43:37 -0300

    feat(client): add featured product image to homepage

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,3 +1,11 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <div className="">homepage</div>;
+  return (
+    <div className="">
+      <div className="relative aspect-3/1 mb-12">
+        <Image src="/featured.png" alt="Feartured Product" fill />
+      </div>
+    </div>
+  );
 }
