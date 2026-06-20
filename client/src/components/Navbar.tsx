598f400426ce830e97dfde4b0ec8f6aab17cb364
Pedro Casseb <casseb.phcc@gmail.com>
2026-06-20 16:36:34 -0300

    refactor(Navbar): use ShoppingCartIcon component

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
 import SearchBar from "./SearchBar";
-import { Bell, Home, ShoppingCart } from "lucide-react";
+import { Bell, Home } from "lucide-react";
+import ShoppingCartIcon from "./ShoppingCartIcon";
 
 export default function Navbar() {
   return (
@@ -25,7 +26,7 @@ export default function Navbar() {
           <Home className="size-4 text-gray-600" />
         </Link>
         <Bell className="size-4 text-gray-600" />
-        <ShoppingCart className="size-4 text-gray-600" />
+        <ShoppingCartIcon />
         <Link href="/login">Sign In</Link>
       </div>
       {/* Right */}
